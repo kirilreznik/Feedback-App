919487dda47e992723dab2f91a406cdfe2c03e59
kirilreznik <kirilreznik@gmail.com>
2022-05-01 18:38:04 +0300

    update README.md

--- src/pages/about-page/AboutPage.jsx
+++ src/pages/about-page/AboutPage.jsx
@@ -10,7 +10,21 @@ const AboutPage = () => {
       <Card>
         <div className="about">
           <h1>About this project</h1>
-          <p>React feedback app</p>
+          <p>I have built this app as a part of React course</p>
+          <p>Topics covered in the course:</p>
+          <p>
+            -React Components <br />
+            -JSX Props (proptypes, defaultprops, etc)
+            <br />
+            -State (Component & App Level)
+            <br /> -Styling
+            <br />
+            -Handling Events
+            <br /> -Lists & Keys <br />
+            -Forms <br />
+            -Context API <br />
+            -HTTP Requests
+          </p>
           <p>Version : 1.0.0</p>
           <Link to="/">Back Home</Link>
         </div>
